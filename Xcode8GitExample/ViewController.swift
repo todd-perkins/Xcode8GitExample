46461bdc47360992be2df1c7b9e98f691adaae26
Todd Perkins <toddperkins@todds-imac.fios-router.home>
2016-10-05 14:14:47 -0700

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Todd Perkins on 10/5/16.
+//  Copyright © 2016 Todd Perkins. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
